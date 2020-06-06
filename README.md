56b11cf56958cae24341568d00e64f5d1e7812e2
solnsumei <solnsumei@gmail.com>
2020-06-06 18:49:32 +0100

    App setup and codebase

---
+++ README.md
@@ -0,0 +1,6 @@
+# Dice Flutter
+
+Flutter Dice Rolling Application
+
+## How to play
+- Tap on any of the dice images to roll
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.black,
+        ),
+        body: DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void rollDice() async {
+    int count = 0;
+    final max = 6;
+
+    while(count < 10) {
+      leftDiceNumber = Random().nextInt(max) + 1;
+      rightDiceNumber = Random().nextInt(max) + 1;
+
+      await Future.delayed(const Duration(milliseconds: 100), () {
+        setState(() {});
+      });
+
+      count++;
+    }
+  }
+
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                rollDice();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                rollDice();
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
